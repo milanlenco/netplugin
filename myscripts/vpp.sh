bbaa0dbb04006da847e032b932afc08c2813d2f1
Milan Lenco -X (milenco - PANTHEON TECHNOLOGIES at Cisco) <milenco@cisco.com>
2017-08-17 06:16:58 -0700

    Hardening myscripts/vpp.sh (not for pull request)

--- myscripts/vpp.sh
+++ myscripts/vpp.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # Configure VPP
-if pidof vpp; then
-       sudo kill `pidof vpp`
-       sleep 5
-fi
-sudo vpp "api-trace { on } dpdk { dev 0000:00:0a.0 }"
-sleep 5
+while sudo vppctl sh version | grep FileNotFoundError;
+do
+	echo "Restarting dysfunctional VPP..."
+	sudo systemctl disable vpp.service # disable auto-restarts
+	#sudo service vpp stop
+	#sleep 3
+	if pidof vpp; then
+		sudo kill `pidof vpp`
+		sleep 3
+	fi
+	sudo rm -f /dev/shm/vpe-api
+	sudo ./dpdk.py -u 0000:00:0a.0
+	sudo modprobe uio_pci_generic
+	sudo ./dpdk.py -b uio_pci_generic 0000:00:0a.0
+	sudo vpp "api-trace { on } dpdk { dev 0000:00:0a.0 }"
+	sleep 5
+done
 if [[ $(hostname) == "netplugin-node1" ]]; then
        sudo vppctl set int ip address GigabitEthernet0/a/0 192.168.2.10/24
 else
